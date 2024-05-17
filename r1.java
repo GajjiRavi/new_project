bef6a491a2b31676afcaffe43f59f3d56a1b146c
GajjiRavi <133233198+GajjiRavi@users.noreply.github.com>
2024-05-17 22:42:29 +0530

    Update r1.java

--- r1.java
+++ r1.java
@@ -1,9 +1,6 @@
 public class r1 {
     public static void main(String args[]) {
-          int x=10;
-	        int y=25;
-		      int z=x+y;
-
-		            System.out.println("Sum of x+y = " + z);
+         
+		            System.out.println("Sum of x+y = ");
 			        }
 				}
